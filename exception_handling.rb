ebd28c43b3683ac52a9d5988f8a02ebf432d67d2
Haani Niyaz <Haani@Haanis-MacBook-Air.local>
2014-04-01 20:20:33 +1100

    basics of exception handling

---
+++ exception_handling.rb
@@ -0,0 +1,22 @@
+
+def divide dividend, divisor
+	# question mark is part of the method
+	if !dividend.is_a?(Fixnum) || !divisor.is_a?(Fixnum)
+		raise ArgumentError, "You'll need to provide 2 numbers"
+  end
+
+  if divisor == 0
+  	raise StandardError, "You're trying to divide by 0"
+  end
+
+  dividend/divisor
+
+ end
+
+begin
+	puts divide 5, 0
+
+# capture Standard and Argument error
+rescue StandardError => e 
+	puts e.message
+end
