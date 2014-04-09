99ad5266803f93d91e4f6cc80e3ad28a54d6c0a0
Haani Niyaz <haani.niyaz@gmail.com>
2014-04-09 19:41:43 +1000

    example of using arrays

---
+++ arrays.rb
@@ -0,0 +1,44 @@
+class Post
+	def initialize title, body, author
+		@title  = title
+		@body   = body
+		@author = author
+	end
+
+	def title
+		@title
+	end
+end
+
+
+# array of objects
+
+posts = [
+	Post.new('First post', 'Some text', 'bob the builder'),
+	Post.new('Second post', 'Some second text', 'spiderman'),
+	Post.new('Third post', 'Some thirdly text', 'batman'),
+]
+
+# block declartion to get post object and print title
+
+puts posts.map { |post| post.title }
+
+# push to array
+
+last_post = Post.new('Fourth post', 'Some fourth text', 'batman')
+
+posts.push(last_post) # also can use posts << last_post
+
+# size or length of array - you can also use size or length instead
+
+puts(posts.count)
+
+# checks if a given object is present in the array
+
+puts posts.include? last_post
+
+
+# returns a new array containing all objects matching condition
+
+puts posts.select { |post| post. title == 'First post' }
+
