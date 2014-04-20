ebb14768133d2273e3469cadf66cdcacb08a391e
Haani Niyaz <haani.niyaz@gmail.com>
2014-04-21 09:33:47 +1000

    abstracted functions to accept any text to be ignored

--- scripting-with-ruby/differences.rb
+++ scripting-with-ruby/differences.rb
@@ -1,21 +1,46 @@
-old_inventory = File.open("code/inventory/old-inventory.txt").readlines
-new_inventory = File.open("code/inventory/new-inventory.txt").readlines
+def check_usage   
+  unless ARGV.length == 2 
+    puts "Usage: differences.rb old-inventory new-inventory"
+    exit
+  end
+end
+
+def boring?(line,boring_words)
+ boring_words.any? do |word|
+  contains?(line,word) 
+ end
+end
 
 
-new_count = (new_inventory - old_inventory).length
-old_count = (old_inventory - new_inventory).length
+def contains?(haystack,needle)
+  haystack.split('/').include?(needle) 
+end  
 
+def inventory_from(filename)
+  inventory = File.open(filename)
+  downcased = inventory.collect do | line | 
+    line.downcase
+  end
 
-puts 'The following files bave been added:'
-puts new_inventory - old_inventory
-puts "The total number of new files: #{new_count}"
+# removing  '\n' with chomp
+  downcased.reject do | line |
+    boring?(line.chomp,['temp','recycler'])
+  end
+end
 
-puts ''
-puts 'The following files have been deleted:'
-puts old_inventory - new_inventory
-puts "The total number of deleted files: #{old_count}"
-puts ''
-puts "The number of common files:  #{(new_inventory & old_inventory).length}"
+def compare_inventory_files(old_file, new_file) # (2)
+  old_inventory = inventory_from(old_file)
+  new_inventory = inventory_from(new_file)
 
+  puts "The following files have been added:"
+  puts new_inventory - old_inventory
 
+  puts ""
+  puts "The following files have been deleted:"
+  puts old_inventory - new_inventory
+end
 
+if $0 == __FILE__ # (3)
+  check_usage 
+  compare_inventory_files(ARGV[0], ARGV[1]) 
+end
